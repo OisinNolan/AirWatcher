e6ebdffcfc70f850ecfc10a51a48acb0b89f29cc
aakaydin <37652644+aakaydin@users.noreply.github.com>
2020-05-29 16:39:22 +0300

    Add files via upload

---
+++ AirCleaner.h
@@ -0,0 +1,29 @@
+#ifndef AIRCLEANER_H 
+#define AIRCLEANER_H
+
+#include <string>   
+
+class AirCleaner {
+        
+    public:
+        string cleanerId;
+        double latitude;
+        double longitude;
+        string start;
+        string end;
+        string description;
+
+
+        AirCleaner( string id, double lat, double lon, string start, string end, string desc ){
+            cleanerId = id;
+            latitude = lat;
+            longitude = lon;
+            this->start = start;
+            this->end = end;
+            description = desc;
+
+        }
+
+}
+
+#endif
---
+++ Zone.h
@@ -0,0 +1,30 @@
+#ifndef ZONE_H 
+#define ZONE_H
+
+#include <string>   
+
+class Zone {
+        
+    public:
+        double latitude;
+        double longitude;
+        double radius;
+        
+        
+
+
+        Zone( double lat, double lon, double rad ){
+            latitude = lat;
+            longitude = lon;
+            radius = rad;
+        }
+}
+
+//Check if this works
+inline bool Zone::IsWithin( double lat, double lon ){
+
+    return radius*radius > (latitude-lat)*(latitude-lat) + (longitude-lon)*(longitude-lon) ;
+}
+
+
+#endif
