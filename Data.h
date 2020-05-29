96e7808e36540ee8d74f9eb15a886210dd936417
Oisín <oinolan@tcd.ie>
2020-05-29 14:11:36 +0100

    sensor and data headers added

---
+++ Data.h
@@ -0,0 +1,19 @@
+/*
+
+*/
+#ifndef DATA_H
+#define DATA_H
+
+#include <string>
+
+using namespace std;
+
+class Data {
+public:
+    string time;
+    string sensorId;
+    string type;
+    double value;
+};
+
+#endif
---
+++ Sensor.h
@@ -0,0 +1,19 @@
+/*
+
+*/
+#ifndef DATA_H
+#define DATA_H
+
+#include <string>
+
+using namespace std;
+
+class Data {
+public:
+    string id;
+    double latitude;
+    double longitude;
+    string description;
+};
+
+#endif
